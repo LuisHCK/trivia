07f2a1728a53ae6e58d4fdc80be2c415626e7262
Luis J. Centeno <luisjcenteno@live.com>
2021-05-17 08:45:26 -0600

    🩹 Remove console log

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -19,7 +19,7 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-console.log(serviceWorkerRegistration.register())
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
